2a777f4e1d7af9d96aa3c9e2a48813c1acba4b87
l_yunwen@qq.com <l_yunwen@qq.com>
2019-03-20 17:14:26 +0800

    ~

--- common/func_file.go
+++ common/func_file.go
@@ -2,15 +2,23 @@ package common
 
 import (
 	"os"
+	"strings"
 	"time"
 )
 
 //日志方法
-func Log(log string) {
-	log = time.Now().Format("2006-01-02 15:04:05") + "|" + log
+func Log(log string) error {
 	f, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
-		return
+		return err
 	}
-	f.WriteString(log)
+	//_, err = f.WriteString(log)
+
+	fd_time := time.Now().Format("2006-01-02 15:04:05")
+	log = strings.Join([]string{"======", fd_time, "=====", log, "\n"}, "")
+	buf := []byte(log)
+	f.Write(buf)
+	f.Close()
+
+	return err
 }
